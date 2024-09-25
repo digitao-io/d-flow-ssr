69c96376240da73e8a044ccab6b5334e710fac7f
digitao-io <contact@digitao.io>
2024-09-25 13:57:29 +0200

    Updated the eslint rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,7 @@ export default [
       ...stylisticEslintPlugin.configs["recommended-flat"].rules,
 
       "@typescript-eslint/no-non-null-assertion": ["off"],
+      "@stylistic/arrow-parens": ["error", "always"],
       "@stylistic/member-delimiter-style": ["error", {
         "multiline": { "delimiter": "semi", "requireLast": true },
         "singleline": { "delimiter": "semi", "requireLast": false },
